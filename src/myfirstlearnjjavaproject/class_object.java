c658e0fda4ab1310cf91b3f9f41da679dbf907c1
08ppty <pemmarajupavan08@gmail.com>
2023-10-30 20:21:55 +0000

    commit for do wfor classes and object

---
+++ src/myfirstlearnjjavaproject/class_object.java
@@ -0,0 +1,31 @@
+package myfirstlearnjjavaproject;
+
+public class class_object {
+
+	
+	
+
+		// sw=software
+		static String sw_name;
+		static float sw_price;
+
+		static void set(String n, float p)
+		{
+			sw_name = n;
+			sw_price = p;
+		}
+
+		static void get()
+		{
+			System.out.println("Software name is: " + sw_name);
+			System.out.println("Software price is: "
+							+ sw_price);
+		}
+
+		public static void main(String args[])
+		{
+			class_object.set("Visual studio", 0.0f);
+			class_object.get();
+		}
+	}
+;
