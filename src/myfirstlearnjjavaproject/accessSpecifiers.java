6a84c7480fcfbe76c4652ca2b01d19a030af5b1e
08ppty <pemmarajupavan08@gmail.com>
2023-10-30 19:54:46 +0000

    commitst

---
+++ src/myfirstlearnjjavaproject/accessSpecifiers.java
@@ -0,0 +1,17 @@
+package myfirstlearnjjavaproject;
+
+public class accessSpecifiers {
+
+	
+
+	public static void main(String[] args) {
+		//default
+		System.out.println("Dafault Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		  
+        obj.display(); 
+
+	}
+
+	
+	
+}
---
+++ src/myfirstlearnjjavaproject/priaccessspecifier.java
@@ -0,0 +1,9 @@
+package myfirstlearnjjavaproject;
+
+public class priaccessspecifier {
+	   private void display() 
+	    { 
+	        System.out.println("You are using private access specifier"); 
+	    
+
+}
