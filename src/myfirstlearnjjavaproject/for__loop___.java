801186a9e96131f0fce5cf0d5c33d4537fc41c79
08ppty <pemmarajupavan08@gmail.com>
2023-10-30 20:07:49 +0000

    commit for do wfor loop

---
+++ src/myfirstlearnjjavaproject/for__loop___.java
@@ -0,0 +1,15 @@
+package myfirstlearnjjavaproject;
+
+public class for__loop___ {
+
+	
+	
+	  
+	    public static void main(String[] args) 
+	    { 
+	        for (int i = 1; i <= 10; i++) { 
+	            System.out.println(i); 
+	        } 
+	    } 
+	}
+
