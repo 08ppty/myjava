c41685eca7818bb78fef27989bdd1389120dfada
08ppty <pemmarajupavan08@gmail.com>
2023-10-31 03:07:57 +0000

    commit for generic

---
+++ src/myfirstlearnjjavaproject/inher__.java
@@ -0,0 +1,23 @@
+package myfirstlearnjjavaproject;
+
+public class inher__ {
+	
+    class Employee  
+    {    
+    float salary=34534*12;    
+    }    
+    public class Executive extends Employee  
+    {   
+    float bonus=3000*6;  
+   
+    Executive obj=new Executive();   
+    System.out.println("Total salary credited: "+obj.salary);    
+    System.out.println("Bonus of six months: "+obj.bonus);   
+     }}; 
+    
+	
+	
+	
+	
+	
+
